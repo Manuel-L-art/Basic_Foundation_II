49a35081eb386ca06a82c9a4ad0dc8d27480a8f1
Manuel Lopez <lopez1manuel@gmail.com>
2021-03-15 15:03:30 -0700

    added 4th challenge, forgot in last commit

--- Basic_Foundation_II.js
+++ Basic_Foundation_II.js
@@ -33,3 +33,27 @@ console.log(prtLowRtrnHigh(x));
 
 // 3. Print One, Return Another
 
+function prtOneRtrnAther(arr) {
+    console.log(arr[arr.length - 2]);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !=0) {
+            return arr[i];
+        }
+    }
+}
+var x = [12,3,4,4,5,32,3,43,534,-43,4345];
+console.log(prtOneRtrnAther(x));
+
+// 4. Double Vision 
+
+function doubleArr(arr) {
+    var j = [];
+    for( var i = 0; i < arr.length; i++) {
+        j.push(arr[i]*2)
+    }
+    return j;
+}
+
+var x = [3,4,5,6,435,34,43,5,-43,434];
+console.log(doubleArr(x));
+
