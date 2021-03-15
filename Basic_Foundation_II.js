be9e3473feba8b61994370c7d9c8367c875fda1c
Manuel Lopez <lopez1manuel@gmail.com>
2021-03-15 15:01:19 -0700

    1-4 Challenges Completed

---
+++ Basic_Foundation_II.js
@@ -0,0 +1,35 @@
+// 1. Biggie Size
+
+function positBig(arr) {
+    for( var i = 0; i< arr.length; i++) {
+        if ( arr[i] > 0) {
+            arr[i] = "big";
+        }
+    }
+    return arr;
+}
+var x = [1,3,5,344,-76,435,-43,345];
+
+console.log(positBig(x));
+
+// 2. Print Low, Return High
+
+function prtLowRtrnHigh(arr) {
+    var low = arr[0];
+    var high = 0;
+    for(var i = 0; i < arr.length; i++) {
+        if (arr[i] < low) {
+            low = arr[i];
+        }
+        if (arr[i] > high) {
+            high = arr[i];
+        }
+    }
+    console.log(low);
+    return high;
+}
+var x = [2,4,56,6,5,34,5,-4];
+console.log(prtLowRtrnHigh(x));
+
+// 3. Print One, Return Another
+
